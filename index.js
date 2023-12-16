32bf3e2125b2afd3e4161b39bd41d663c79ba809
David05 <davidgreendevlops@gmail.com>
2023-12-16 15:03:14 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const menuIcon = document.querySelector(".menu-icon");
 const linear = document.getElementById("hide");
 const cancelButton = document.getElementById("cancel");
 const blockNav = document.getElementById("block");
+const spec = document.querySelector(".special")
+
+
+
+
 
 menuIcon.addEventListener('click', () => {
     linear.classList = "show";
     cancelButton.classList = "text-white position-absolute top-0";
     blockNav.classList = "block";
+    innerWidth  <= 750 && spec.addEventListener("click", ()=>{
+        linear.classList = "d-lg-flex justify-content-between gap-3";
+        cancelButton.classList = "hide";
+        blockNav.classList = "d-none d-lg-flex justify-content-between gap-3";
+    })
 });
 
 cancelButton.addEventListener('click', () => {
